e4ae99b24de78031ec8c38c383b498958f0f1bf9
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-07-07 18:18:23 +0200

    checkstyle errors fixed

--- src/main/java/org/everit/blobstore/mem/internal/BlobAccessorAndLock.java
+++ src/main/java/org/everit/blobstore/mem/internal/BlobAccessorAndLock.java
@@ -2,6 +2,11 @@ package org.everit.blobstore.mem.internal;
 
 import java.util.concurrent.locks.Lock;
 
+/**
+ * Holder class for blob accessor and a lock that is held for the blob. Both can be
+ * <code>null</code> depending on the use-case.
+ *
+ */
 public class BlobAccessorAndLock {
 
   public final MemBlobAccessorImpl blobAccessor;
